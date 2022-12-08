ac9fec17f0f1be0da668f1fac099e06123b45f5c
Mario Zenuni <mariozenuni@gm,ail>
2022-12-08 12:00:47 +0100

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
